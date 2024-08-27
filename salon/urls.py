4e08bb98f2353a9154d669a27291c7c239093e4c
thuantm <thuantm@flydinotech.com>
2024-08-27 17:15:48 -0400

    - add api

--- salon/urls.py
+++ salon/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import (
+    EmployeeViewSet,
+    UserViewSet,
+    SkillViewSet,
+    NailServiceCategoryViewSet,
+    NailServiceViewSet,
+    AppointmentViewSet
+)
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'skills', SkillViewSet)
+router.register(r'nailservicecategories', NailServiceCategoryViewSet)
+router.register(r'nailservices', NailServiceViewSet)
+router.register(r'appointments', AppointmentViewSet)
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
